a3379aca2b3dfd367a7a4466bcaf2219f38b4ada
Matt Westcott <matt@west.co.tt>
2020-11-18 16:08:09 +0000

    Add HTML cleaning to PageBlock

--- wagtail_webstories/blocks.py
+++ wagtail_webstories/blocks.py
@@ -1,6 +1,12 @@
 from wagtail.core import blocks
+from webstory import StoryPage
+
+
+class AMPCleanHTMLBlock(blocks.RawHTMLBlock):
+    def clean(self, value):
+        return StoryPage.clean_html_fragment(value or '')
 
 
 class PageBlock(blocks.StructBlock):
     id = blocks.CharBlock()
-    html = blocks.RawHTMLBlock()
+    html = AMPCleanHTMLBlock()
