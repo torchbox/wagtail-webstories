7d3134c40e3347dbb9d5e0cdb2eb67771e904d36
Matt Westcott <matt@west.co.tt>
2020-11-19 16:38:14 +0000

    add tests for embedding

--- tests/models.py
+++ tests/models.py
@@ -1,4 +1,23 @@
+from wagtail.admin.edit_handlers import StreamFieldPanel
+from wagtail.core import blocks
+from wagtail.core.fields import StreamField
+from wagtail.core.models import Page
+
+from wagtail_webstories.blocks import StoryChooserBlock, StoryEmbedBlock
 from wagtail_webstories.models import BaseWebStoryPage
+
 
 class StoryPage(BaseWebStoryPage):
     pass
+
+
+class BlogPage(Page):
+    body = StreamField([
+        ('heading', blocks.CharBlock()),
+        ('story_embed', StoryEmbedBlock()),
+        ('story_link', StoryChooserBlock()),
+    ])
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('body'),
+    ]
---
+++ tests/tests/test_embedding.py
@@ -0,0 +1,75 @@
+from django.test import TestCase
+from wagtail.core.models import Site
+
+from tests.models import BlogPage, StoryPage
+
+
+class TestEmbedding(TestCase):
+    def setUp(self):
+        self.home = Site.objects.get().root_page
+        self.story_page = StoryPage(
+            title="Wagtail spotting",
+            slug="wagtail-spotting",
+            publisher="Torchbox",
+            publisher_logo_src="https://example.com/torchbox.png",
+            poster_portrait_src="https://example.com/wagtails.jpg",
+        )
+        self.story_page.custom_css = """
+            #cover {background-color: #eee;}
+        """
+        self.story_page.pages = [
+            ('page', {
+                'id': 'cover',
+                'html': """
+                    <amp-story-page id="cover">
+                        <amp-story-grid-layer template="vertical">
+                            <h1>Wagtail spotting</h1>
+                        </amp-story-grid-layer>
+                    </amp-story-page>
+                """
+            }),
+            ('page', {
+                'id': 'page-1',
+                'html': """
+                    <amp-story-page id="page-1">
+                        <amp-story-grid-layer template="vertical">
+                            <p>Today we went out wagtail spotting</p>
+                        </amp-story-grid-layer>
+                    </amp-story-page>
+                """
+            }),
+        ]
+        self.home.add_child(instance=self.story_page)
+
+    def test_render_with_embed(self):
+        blog_page = BlogPage(
+            title="November nature notes",
+            slug="november-nature-notes",
+        )
+        blog_page.body = [
+            ('heading', "Story of the week"),
+            ('story_embed', self.story_page),
+        ]
+        self.home.add_child(instance=blog_page)
+
+        response = self.client.get('/november-nature-notes/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, '<amp-story-player>')
+        self.assertContains(response, '<a href="/wagtail-spotting/">')
+
+    def test_render_with_link(self):
+        blog_page = BlogPage(
+            title="November nature notes",
+            slug="november-nature-notes",
+        )
+        blog_page.body = [
+            ('heading', "Story of the week"),
+            ('story_link', self.story_page),
+        ]
+        self.home.add_child(instance=blog_page)
+
+        response = self.client.get('/november-nature-notes/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'href="/wagtail-spotting/"')
+        self.assertContains(response, '"background-image: url(https://example.com/wagtails.jpg);"')
+        self.assertContains(response, '<div class="title">Wagtail spotting</div>')
