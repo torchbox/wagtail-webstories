2744634595349e9f98a8a85bdb078a3ab5dfb25d
Matt Westcott <matt@west.co.tt>
2020-11-20 19:41:20 +0000

    Add methods for importing metadata images

--- wagtail_webstories/models.py
+++ wagtail_webstories/models.py
@@ -1,14 +1,20 @@
+import hashlib
 import json
+from urllib.parse import urlparse
 
 from django.apps import apps
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+import requests
 from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel, StreamFieldPanel
 from wagtail.core.fields import StreamField
+from django.core.files.base import ContentFile
+from django.core.files.images import ImageFile
 from wagtail.core.models import Page, get_page_models
 from wagtail.images import get_image_model_string
 from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.images.models import Filter
 
 from .blocks import PageBlock
 
@@ -129,6 +135,77 @@ class BaseWebStoryPage(Page):
         context = super().get_context(request)
         context['ld_json'] = json.dumps(self.linked_data)
         return context
+
+    def _import_metadata_images(self):
+        if self.publisher_logo_src_original and not self.publisher_logo:
+            self.publisher_logo, created = self._image_from_url(
+                self.publisher_logo_src_original,
+                title="%s logo" % self.publisher,
+            )
+
+        if self.poster_portrait_src_original and not self.poster_image:
+            portrait_image_file = self._image_file_from_url(self.poster_portrait_src_original)
+            self.poster_image, created = self._image_from_image_file(
+                portrait_image_file, title=self.title
+            )
+
+            if created:
+                # Pre-generate renditions for whichever of portrait, square and landscape poster images
+                # have been supplied, indexed under PORTRAIT_IMAGE_FILTER, SQUARE_IMAGE_FILTER
+                # and LANDSCAPE_IMAGE_FILTER respectively so that when we look those up again we get
+                # the original appropriately-cropped version
+                portrait_filter = Filter(self.PORTRAIT_IMAGE_FILTER)
+                self.poster_image.renditions.create(
+                    filter_spec=self.PORTRAIT_IMAGE_FILTER,
+                    file=portrait_image_file,
+                    focal_point_key=portrait_filter.get_cache_key(self.poster_image)
+                )
+
+                if self.poster_square_src_original:
+                    square_filter = Filter(self.SQUARE_IMAGE_FILTER)
+                    self.poster_image.renditions.create(
+                        filter_spec=self.SQUARE_IMAGE_FILTER,
+                        file=self._image_file_from_url(self.poster_square_src_original),
+                        focal_point_key=square_filter.get_cache_key(self.poster_image)
+                    )
+
+                if self.poster_landscape_src_original:
+                    landscape_filter = Filter(self.LANDSCAPE_IMAGE_FILTER)
+                    self.poster_image.renditions.create(
+                        filter_spec=self.LANDSCAPE_IMAGE_FILTER,
+                        file=self._image_file_from_url(self.poster_landscape_src_original),
+                        focal_point_key=landscape_filter.get_cache_key(self.poster_image)
+                    )
+
+    def _image_file_from_url(self, url):
+        url_obj = urlparse(url)
+        filename = url_obj.path.split('/')[-1] or 'image'
+        response = requests.get(url)
+        return ImageFile(ContentFile(response.content), name=filename)
+
+    def _create_image(self, file, title=None):
+        """
+        Return an unsaved image instance for the given ImageFile and title. Override this if your
+        image model requires extra metadata to be filled in
+        """
+        return Image(file=file, title=title)
+
+    def _image_from_image_file(self, file, title=None):
+        with file.open() as f:
+            file_hash = hashlib.sha1(f.read()).hexdigest()
+
+        # check if we already have an image with the same file content
+        image = Image.objects.filter(file_hash=file_hash).first()
+        if image:
+            return (image, False)
+        else:
+            image = self._create_image(file, title=title)
+            image.save()
+            return (image, True)
+
+    def _image_from_url(self, url, title=None):
+        image_file = self._get_image_file_from_url(url)
+        return self._image_from_image_file(image_file, title=title)
 
     class Meta:
         abstract = True
